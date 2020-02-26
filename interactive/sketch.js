d37ceb3d32c13b4b65fd46cf7e88bce1d54f03b7
Jznekker <57229353+Jznekker@users.noreply.github.com>
2020-02-26 13:48:08 -0500

    fix

--- interactive/sketch.js
+++ interactive/sketch.js
@@ -1,5 +1,7 @@
 var r = 0;
 var b = 255;
+var eye = 60;
+
 
 function setup() {
 	createCanvas(600, 400);
@@ -9,34 +11,38 @@ function draw() {
 	//background
 	//r = map(mouseX, 0, 600, 0, 255);
 	//b = map(mouseY, 0, 600, 255, 0);
+    
+    var x = pmouseX;
+    var y = pmouseY;
 
-	background('white');
+	background('black');
 
 	//Face
 	r = map(mouseX, 0, 600, 0, 255);
 	b = map(mouseY, 0, 600, 255, 0);
 
-	fill(r, 0, b);
-	ellipse(320, 180, 300);
+	fill('yellow');
+	ellipse(x, y, 300);
 	
 	//Right eye
-	fill('black');
-	ellipse(400, 160, 50);
-	//right iris
 	fill('white');
+	ellipse(x + 80, y, eye);
+	//right iris
+	fill(r, 0, b);
 	ellipse(400, 160, 40);
 
 	//Left eye
-	fill('black');
-	ellipse(240, 160, 50);
-	//left iris
 	fill('white');
+	ellipse(x - 80, y, eye);
+	//left iris
+	fill(r, 0, b);
 	ellipse(240, 160, 40);
 	//mouth
 	fill('black');
-	ellipse(320, 230, 50, 80);
+	ellipse(x + 5, y + 60, 50, 80);
 
 	//tongue
-	fill('red');
+	fill('white');
 	ellipse(320, 250, 40, 40);
+    
 }
